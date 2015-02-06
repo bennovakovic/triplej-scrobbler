c1868283d6e4f3f8a7d7b6753a096cff87bb8805
Ben Novakovic <bmn@bmn.name>
2015-02-06 16:24:17 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # triplej-scrobbler
 A little glue package to scrobble radio listens from my sonos.
+
+
+This assumes that that you're running node-sonos-http-api on the same box.
+
+https://github.com/jishi/node-sonos-http-api
---
+++ app.js
@@ -0,0 +1,30 @@
+var config = require('./config');
+var utf8 = require('utf8');
+var fs = require('fs');
+var http = require('http');
+
+var options = {
+  host: config.sonos_server,
+  path: '/zones'
+};
+
+
+var processZoneResponse = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+    var d = JSON.parse(str);
+    console.log(d);
+  });
+}
+
+
+
+http.request(options, processZoneResponse).end();
